23f09a49ed288238eed02377d2b7485afd3f4a6b
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-05-05 20:56:31 -0300

    fix: dropping swagger

--- src/routes/jordan.route.js
+++ src/routes/jordan.route.js
@@ -1,12 +1,12 @@
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../../swagger.json')
+// const swaggerDocument = require('../../swagger.json')
 const route = require("express").Router();
 const controllerBoots = require("../controllers/jordan.controller");
 const {idValidation, objectBodyValidation} = require('../middlewares/boot.middleware')
 
 //Rotas Swagger
-route.use('/api-docs', swaggerUi.serve);
-route.get('/api-docs',swaggerUi.setup(swaggerDocument));
+// route.use('/api-docs', swaggerUi.serve);
+// route.get('/api-docs',swaggerUi.setup(swaggerDocument));
 
 route.get("/all-boots",controllerBoots.findAllBootsController);
 route.get("/find-boot/:id", idValidation, controllerBoots.findByIdBootController);
